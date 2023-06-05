3fee90c28d9cfa4ee664181cc2eb9319fd7ad471
Aleceason199 <aleceason@hotmail.com>
2023-06-05 23:27:40 +1000

    added comments

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const body = document.querySelector("body");
-
 const divContainer = document.createElement("div");
 divContainer.className = "divContainer"
-
 body.appendChild(divContainer);
 
 
+//inputs and button for start div
 const boxesInput = document.querySelector("#boxes");
 const boxSizeInput = document.querySelector("#boxSize");
-
 const startButton = document.querySelector("#startButton");
 startButton.addEventListener('click', () => {createSketchPad(boxesInput.value, boxSizeInput.value)});
 
@@ -22,11 +20,13 @@ function mouseHovered(id){
 //creates the sketchpad with the inputed numbers
 function createSketchPad(boxes, boxSize){
 
+    //removes any containers for reset
     var etches = divContainer.querySelectorAll("div");
     etches.forEach((etch) => {
         etch.remove()
     });
 
+    //creates the amount of boxes at the size and gives them each an id
     for (var i = 0; i < boxes; i++){
         divBox = document.createElement("div");
         divBox.style.padding = boxSize + "px " + boxSize + "px";
@@ -37,6 +37,7 @@ function createSketchPad(boxes, boxSize){
     
     var etches = divContainer.querySelectorAll("div");
     
+    //creates an event listener to change the color of a div when moused over
     etches.forEach((etch) => {
         etch.addEventListener('mouseover', () => {mouseHovered(etch.id)});
      });
